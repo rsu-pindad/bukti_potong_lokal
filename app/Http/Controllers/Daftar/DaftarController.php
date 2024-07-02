27abab9056433ba87c5786eec3ddd88e738a54d3
rsu-pindad <sautomated01@gmail.com>
2024-07-02 13:12:05 +0700

    initial daftar user setelah cari npp

---
+++ app/Http/Controllers/Daftar/DaftarController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Daftar;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\View\View;
+use RealRashid\SweetAlert\Facades\Alert;
+
+class DaftarController extends Controller
+{
+    // protected $redirectRoute = '';
+
+    public function index(Request $request): View
+    {
+        // dd($request->old('npp'));
+        return view('guest.daftar')->with([
+            'route'    => 'daftar',
+            'showSelf' => true,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|string|unique:tbl_user|min:5|max:15',
+            'password' => 'required|confirmed|min:6',
+        ]);
+
+        $request->session()->reflash();
+
+        if ($validator->fails()) {
+            return redirect('daftar')
+                       ->withErrors($validator)
+                       ->withInput();
+        }
+
+        try {
+            $user           = new User;
+            $user->username = $validator->safe()->username;
+            $user->password = Hash::make($validator->safe()->password);
+            $user->save();
+            flash()
+                ->success('akun berhasil di buat, silahkan masuk')
+                ->flash();
+
+            return redirect('login');
+        } catch (\Throwable $th) {
+            flash()
+                ->error($th->getMessage())
+                ->flash();
+
+            return redirect()->back()->withInput();
+        }
+    }
+}
