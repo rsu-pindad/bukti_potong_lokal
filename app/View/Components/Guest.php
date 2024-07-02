bd76541227a60ce7a2576140be6b840e73443522
rsu-pindad <sautomated01@gmail.com>
2024-07-02 13:13:52 +0700

    initial component guest

---
+++ app/View/Components/Guest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+use Closure;
+
+class Guest extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct() {}
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.guest');
+    }
+}
