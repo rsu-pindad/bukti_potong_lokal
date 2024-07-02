5167110e1c483b5ce46314366ac19100c81991e5
rsu-pindad <sautomated01@gmail.com>
2024-07-02 13:17:10 +0700

    add cari daftar karyawan
    routes

--- routes/web.php
+++ routes/web.php
@@ -2,30 +2,57 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Daftar\CariController;
+use App\Http\Controllers\Daftar\DaftarController;
+use App\Http\Controllers\Employee\EmployeeController;
 use App\Http\Controllers\GajiController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\PPH21Controller;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Maatwebsite\Excel\Facades\Excel;
+
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+ * |--------------------------------------------------------------------------
+ * | Web Routes
+ * |--------------------------------------------------------------------------
+ * |
+ * | Here is where you can register web routes for your application. These
+ * | routes are loaded by the RouteServiceProvider and all of them will
+ * | be assigned to the "web" middleware group. Make something great!
+ * |
+ */
 
-Route::controller(LoginController::class)->middleware('guest')->group(function () {
-    Route::get('/', 'index')->name('auth/login');
+Route::middleware(['guest'])->group(function () {
+    Route::controller(LoginController::class)->group(function () {
+        Route::get('/', 'index')->name('auth/login');
+        Route::get('/login', 'index')->name('login');
 
-    Route::post('authenticate', 'authenticate')->name('auth/authenticate');
+        Route::post('authenticate', 'authenticate')->name('auth/authenticate');
+    });
+
+    Route::controller(DaftarController::class)->group(function () {
+        Route::prefix('daftar')->group(function () {
+            Route::get('/', 'index')->name('daftar-index');
+            // Route::get('/', 'create')->name('daftar-create');
+            Route::post('/', 'store')->name('daftar-store');
+        });
+    });
+    Route::controller(CariController::class)->group(function () {
+        Route::prefix('cari')->group(function () {
+            Route::get('/', 'index')->name('cari-index');
+            Route::post('/', 'search')->name('cari-npp');
+        });
+    });
 });
 
 Route::middleware(['auth'])->group(function () {
+    Route::controller(EmployeeController::class)->group(function () {
+        Route::get('employee', 'index')->name('employee');
+        Route::patch('employee', 'edit')->name('employee-edit');
+        Route::patch('employee/pribadi', 'editPribadi')->name('employee-edit-pribadi');
+    });
+
     Route::controller(PegawaiController::class)->group(function () {
         Route::get('pegawai', 'index')->name('pegawai');
 
@@ -62,7 +89,7 @@ Route::middleware(['auth'])->group(function () {
         Route::get('pph21/detail/{pph21}', 'show')->name('pph21/detail');
 
         Route::get('pph21/export', 'export')->name('pph21/export');
-        
+
         Route::get('pph21/export-detail/{id}', 'detailExport')->name('pph21/export-detail');
 
         Route::get('pph21/export-detail/{id}', 'detailExport')->name('pph21/export-detail');
