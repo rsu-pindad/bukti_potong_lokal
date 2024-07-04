3440bac50dcc181040088aaf7d9c7ac6dec541ee
rsu-pindad <sautomated01@gmail.com>
2024-07-04 09:18:16 +0700

    fix formatting

--- app/Models/Gaji.php
+++ app/Models/Gaji.php
@@ -3,17 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Model;
 
 class Gaji extends Model
 {
     use HasFactory;
 
-    protected $table = 'tbl_gaji';
-    protected $guarded = ['id'];
+    protected $table   = 'tbl_gaji';
 
+    protected $guarded = ['id'];
 
     public function pegawai(): BelongsTo
     {
