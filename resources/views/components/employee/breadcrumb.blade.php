fec65bfd0b5de16c2bb531391c47587da5a7ad36
rsu-pindad <sautomated01@gmail.com>
2024-07-04 09:20:18 +0700

    fix layout

--- resources/views/components/employee/breadcrumb.blade.php
+++ resources/views/components/employee/breadcrumb.blade.php
@@ -7,7 +7,6 @@
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
             {{$slot}}
-            
             <li class="breadcrumb-item active" aria-current="page">
                 {{$current_page}}
             </li>
