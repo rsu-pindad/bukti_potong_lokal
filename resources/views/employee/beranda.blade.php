5df3b3f80c408647dd9ecaec16f7db1ddccf9b26
rsu-pindad <sautomated01@gmail.com>
2024-07-04 09:21:56 +0700

    add form data profile add driver.js for tour user

--- resources/views/employee/beranda.blade.php
+++ resources/views/employee/beranda.blade.php
@@ -1,77 +1,41 @@
 <x-employee>
 
-    <x-employee.breadcrumb 
-        top_page="beranda"
-        current_page="{{Route::currentRouteName()}}" 
-    >
-    <li class="breadcrumb-item">
-        <a href="#">Beranda</a>
-    </li>
+    <x-employee.breadcrumb top_page="beranda" current_page="{{Route::currentRouteName()}}">
+        <li class="breadcrumb-item">
+            <a href="#">Beranda</a>
+        </li>
     </x-employee.breadcrumb>
-    <x-employee.personal-profile 
-        username_karyawan="{{Auth::user()->username}}" 
-        npp_karyawan="{{Auth::user()->karyawan->npp}}" 
-        nama_karyawan="{{Auth::user()->karyawan->nama}}" 
-        npwp_karyawan="{{Auth::user()->karyawan->npwp}}" 
-        email_karyawan="{{Auth::user()->karyawan->email}}" 
-        notel_karyawan="{{Auth::user()->karyawan->no_tel}}" 
-        st_ptkp_karyawan="{{Auth::user()->karyawan->st_ptkp ?? ''}}" 
-        st_peg_karyawan="{{Auth::user()->karyawan->st_peg ?? ''}}" 
-        bsTargetPegawai="#informasiKepegawaian" 
-        bsTargetIdentitas="#informasiIdentitas"
-    />
+    <x-employee.personal-profile username_karyawan="{{Auth::user()->username}}" npp_karyawan="{{Auth::user()->karyawan->npp}}" nama_karyawan="{{Auth::user()->karyawan->nama}}" npwp_karyawan="{{Auth::user()->karyawan->npwp}}" email_karyawan="{{Auth::user()->karyawan->email}}" notel_karyawan="{{Auth::user()->karyawan->no_tel}}" st_ptkp_karyawan="{{Auth::user()->karyawan->st_ptkp ?? ''}}" st_peg_karyawan="{{Auth::user()->karyawan->st_peg ?? ''}}" bsTargetPegawai="#informasiKepegawaian" bsTargetIdentitas="#informasiIdentitas" />
 
     @pushOnce('modals')
-    <x-modal.default-inner 
-        rootId="informasiIdentitas"
-        rootLabel="Informasi Identitas"
-        >
+    <x-modal.default-inner rootId="informasiIdentitas" rootLabel="Informasi Identitas">
         <form action="{{ route('employee-edit-pribadi')}}" method="post">
             @method('patch')
             @csrf
             <div class="row mb-2">
                 <div class="col-12">
                     <x-forms.floating-labels name="nama" label="Nama">
-                        <x-inputs.input 
-                            id="nama" 
-                            name="nama" 
-                            placeholder="nama..." 
-                            value="{{Auth::user()->karyawan->nama}}"
-                            required />
+                        <x-inputs.input id="nama" name="nama" placeholder="nama..." value="{{Auth::user()->karyawan->nama}}" required />
                     </x-forms.floating-labels>
                 </div>
             </div>
             <div class="row mb-2">
                 <div class="col-12">
                     <x-forms.floating-labels name="email" label="Email">
-                        <x-inputs.input 
-                            type="email"
-                            id="email" 
-                            name="email" 
-                            placeholder="email..." 
-                            value="{{Auth::user()->karyawan->email}}"
-                            required />
+                        <x-inputs.input type="email" id="email" name="email" placeholder="email..." value="{{Auth::user()->karyawan->email}}" required />
                     </x-forms.floating-labels>
                 </div>
             </div>
             <div class="row mb-2">
                 <div class="col-12">
                     <x-forms.floating-labels name="NoTel" label="No Telephone">
-                        <x-inputs.input 
-                            type="tel"
-                            id="notel" 
-                            name="notel" 
-                            placeholder="nomor telephone..." 
-                            value="{{Auth::user()->karyawan->no_tel}}"
-                            required />
+                        <x-inputs.input type="tel" id="notel" name="notel" placeholder="nomor telephone..." value="{{Auth::user()->karyawan->no_tel}}" required />
                     </x-forms.floating-labels>
                 </div>
             </div>
             <div class="row">
-                <div class="col">    
-                    <x-inputs.button 
-                        type="submit"
-                        class="btn btn-primary">
+                <div class="col">
+                    <x-inputs.button type="submit" class="btn btn-primary">
                         Simpan
                     </x-inputs.button>
                 </div>
@@ -80,42 +44,24 @@
     </x-modal.default-inner>
 
     @if(Auth::user()->karyawan->user_edited === false)
-    <x-modal.default-inner 
-        rootId="informasiKepegawaian"
-        rootLabel="Informasi Kepegawaian"
-        >
+    <x-modal.default-inner rootId="informasiKepegawaian" rootLabel="Informasi Kepegawaian">
         <form action="{{ route('employee-edit')}}" method="POST">
             @method('patch')
             @csrf
             <div class="row">
                 <div class="col-12 mb-3">
                     <x-forms.floating-labels name="npp" label="NPP">
-                        <x-inputs.input 
-                            id="npp" 
-                            name="npp" 
-                            class="form-control-plaintext"
-                            value="{{Auth::user()->karyawan->npp}}" 
-                            placeholder="npp...." 
-                            readonly />
+                        <x-inputs.input id="npp" name="npp" class="form-control-plaintext" value="{{Auth::user()->karyawan->npp}}" placeholder="npp...." readonly />
                     </x-forms.floating-labels>
                 </div>
                 <div class="col-12 mb-3">
                     <x-forms.floating-labels name="npwp" label="NPWP">
-                        <x-inputs.input
-                            id="npwp"
-                            name="npwp"
-                            value="{{Auth::user()->karyawan->npwp}}"
-                            placeholder="npwp...."
-                            required
-                        />
+                        <x-inputs.input id="npwp" name="npwp" value="{{Auth::user()->karyawan->npwp}}" placeholder="npwp...." required />
                     </x-forms.floating-labels>
                 </div>
                 <div class="col-12 mb-3">
                     <x-forms.floating-labels name="ptkp" label="PTKP" required>
-                        <x-inputs.select
-                            id="ptkp"
-                            name="ptkp"
-                            >
+                        <x-inputs.select id="ptkp" name="ptkp">
                             <option hidden>Pilih Status PTKP</option>
                             <option value="{{Auth::user()->karyawan->st_ptkp ?? ''}}" selected readonly>{{Auth::user()->karyawan->st_ptkp ?? 'Belum Diisi'}}</option>
                             <option value="TK0">TK0</option>
@@ -131,21 +77,16 @@
                 </div>
                 <div class="col-12 mb-3">
                     <x-forms.floating-labels name="stpeg" label="Status" required>
-                        <x-inputs.select
-                            id="st_peg"
-                            name="st_peg"
-                        >
-                        <option hidden>Pilih Status Karyawan</option>
-                        <option value="{{Auth::user()->karyawan->st_peg ?? ''}}" selected readonly>{{Auth::user()->karyawan->st_peg ?? 'Belum Diisi'}}</option>
-                        <option value="KONTRAK">KONTRAK</option>
-                        <option value="TETAP">TETAP</option>
+                        <x-inputs.select id="st_peg" name="st_peg">
+                            <option hidden>Pilih Status Karyawan</option>
+                            <option value="{{Auth::user()->karyawan->st_peg ?? ''}}" selected readonly>{{Auth::user()->karyawan->st_peg ?? 'Belum Diisi'}}</option>
+                            <option value="KONTRAK">KONTRAK</option>
+                            <option value="TETAP">TETAP</option>
                         </x-inputs.select>
                     </x-forms.floating-labels>
                 </div>
-                <div class="col">    
-                    <x-inputs.button 
-                        type="submit"
-                        class="btn btn-primary">
+                <div class="col">
+                    <x-inputs.button type="submit" class="btn btn-primary">
                         Simpan
                     </x-inputs.button>
                 </div>
@@ -153,6 +94,108 @@
         </form>
     </x-modal.default-inner>
     @endif
+    @endpushOnce
+
+    @pushOnce('scripts')
+    @basset('https://cdn.jsdelivr.net/npm/driver.js@1.0.1/dist/driver.js.iife.js')
+    @endpushOnce
+    @pushOnce('styles')
+    @basset('https://cdn.jsdelivr.net/npm/driver.js@1.0.1/dist/driver.css')
+    @endpushOnce
+
+    @pushOnce('scripts')
+    <script>
+        const driver = window.driver.js.driver;
+
+        const driverObj = driver({
+            showProgress: true
+            , allowClose: false
+            , steps: [{
+                    element: '#judulIdentitasPribadi'
+                    , popover: {
+                        title: 'Identitas Pribadi'
+                        , description: 'Mohon lengkapi data pribadi'
+                    }
+                }
+                , {
+                    element: '.nama_lengkap'
+                    , popover: {
+                        title: 'Nama Lengkap'
+                        , description: 'Mohon isi nama lengkap'
+                    }
+                }
+                , {
+                    element: '.email'
+                    , popover: {
+                        title: 'Email'
+                        , description: 'Mohon isi email'
+                    }
+                }
+                , {
+                    element: '.notel'
+                    , popover: {
+                        title: 'Nomor Telepon'
+                        , description: 'Mohon isi nomor telepon'
+                    }
+                }
+                , {
+                    element: '.editPribadi'
+                    , popover: {
+                        title: 'Edit Pribadi'
+                        , description: 'Gunakan icon berikut'
+                    }
+                }
+                , {
+                    element: '#judulIdentitasKepegawaian'
+                    , popover: {
+                        title: 'Identitas Pegawai'
+                        , description: 'Mohon Lengkapi data pegawai'
+                    }
+                }
+                , {
+                    element: '.karyawan_npwp'
+                    , popover: {
+                        title: 'NPWP'
+                        , description: 'Mohon Isi NPWP 16 Digit'
+                    }
+                }
+                , {
+                    element: '.karyawan_ptkp'
+                    , popover: {
+                        title: 'PTKP'
+                        , description: 'Pilih PTKP'
+                    }
+                }
+                , {
+                    element: '.karyawan_status'
+                    , popover: {
+                        title: 'Status'
+                        , description: 'Pilih Status'
+                    }
+                }
+                , {
+                    element: '.editKepegawaian'
+                    , popover: {
+                        title: 'Edit Kepegawaian'
+                        , description: 'Gunakan icon berikut'
+                    }
+                }
+                , {
+                    element: '#fakturPajak'
+                    , popover: {
+                        title: 'Faktur Pajak'
+                        , description: 'Dokumen Faktur pajak dapat dilihat setelah melengkapi identitas kepegawaian'
+                    }
+                }
+            , ]
+        });
+
+        if (localStorage.getItem('tours') !== 'viewed') {
+            driverObj.drive();
+            localStorage.setItem('tours', 'viewed');
+        }
+
+    </script>
     @endpushOnce
 
 </x-employee>
