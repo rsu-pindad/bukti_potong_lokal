ed0eae69f163cbc19534664092959b5aa7fbed1b
rsu-pindad <sautomated01@gmail.com>
2024-07-04 09:19:26 +0700

    fix layout change package

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -7,16 +7,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS v5.3.1 -->
-    <link href="/vendor/bootstrap-5.3.1/css/bootstrap.min.css" rel="stylesheet">
-    <!-- FontAwesome 6.4.2 CSS -->
-    <link rel="stylesheet" href="/vendor/fontawesome-free-6.4.2-web/css/all.min.css" />
+    <link rel="stylesheet" href="{{ basset(base_path('vendor/twbs/bootstrap/dist/css/bootstrap.min.css')) }}">
+    <link rel="stylesheet" href="{{ basset(base_path('vendor/twbs/bootstrap-icons/font/bootstrap-icons.min.css')) }}">
 
 </head>
 
 <body>
     <main class="row" style="height: 100vh;">
-        <div class="col-8 m-auto">
+        <div class="col-lg-3 col-md-5 col-sm-8 col-xs-12 m-auto p-5">
             <div class="card">
                 <div class="card-header">
                     <h3 class="text-center m-0">PPH21</h3>
@@ -27,7 +25,9 @@
                         <label for="" class="form-label">Username</label>
                         <div class="input-group mb-3">
                             <input type="text" class="form-control @error('username') is-invalid @enderror" name="username" id="username" placeholder="Username" value="{{ old('username') }}">
-                            <span class="input-group-text" id="basic-addon1"><i class="fa-solid fa-user fa-fw text-secondary"></i></span>
+                            <span class="input-group-text" id="basic-addon1">
+                                <i class="bi bi-person-fill"></i>
+                            </span>
                             @error('username')
                             <div class="invalid-feedback">
                                 {{ $message }}
@@ -37,7 +37,9 @@
                         <label for="" class="form-label">Password</label>
                         <div class="input-group mb-3">
                             <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="password" placeholder="Password">
-                            <span class="input-group-text" id="basic-addon2"><i class="fa-solid fa-lock fa-fw text-secondary"></i></span>
+                            <span class="input-group-text" id="basic-addon2">
+                                <i class="bi bi-lock-fill"></i>
+                            </span>
                             @error('password')
                             <div class="invalid-feedback">
                                 {{ $message }}
@@ -52,7 +54,7 @@
             </div>
         </div>
     </main>
-    <script src="/vendor/fontawesome-free-6.4.2/js/all.min.js"></script>
+    <script src="{{ basset(base_path('vendor/twbs/bootstrap/dist/js/bootstrap.min.js')) }}"></script>
 </body>
 
 </html>
