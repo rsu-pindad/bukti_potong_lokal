4eebc5fcdceae3c787f970cd374971e741b7f432
rsu-pindad <sautomated01@gmail.com>
2024-07-08 14:51:51 +0700

    remove unused element

--- resources/views/components/employee/sidebar.blade.php
+++ resources/views/components/employee/sidebar.blade.php
@@ -8,39 +8,16 @@
             <ul class="nav flex-column">
                 <li class="nav-item">
                     <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="#">
-                        <i class="bi bi-house-fill"></i>
+                        <i class="fa-solid fa-house"></i>
                         Beranda
                     </a>
                 </li>
             </ul>
-
-            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase">
-                <span>Laporan Pajak</span>
-                <a class="link-secondary" href="#" aria-label="Buat Laporan">
-                    <i class="bi bi-plus-circle"></i>
-                </a>
-            </h6>
-            <ul class="nav flex-column mb-auto">
-                <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#">
-                        <i class="bi bi-file-earmark-text"></i>
-                        Bulan Ini
-                    </a>
-                </li>
-            </ul>
-
             <hr class="my-3">
-
             <ul class="nav flex-column mb-auto">
-                <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#">
-                        <i class="bi bi-gear-wide-connected"></i>
-                        Pengaturan
-                    </a>
-                </li>
                 <li class="nav-item">
                     <a class="nav-link d-flex align-items-center gap-2" href="{{route('logout')}}">
-                        <i class="bi bi-door-closed"></i>
+                        <i class="fa-solid fa-arrow-right-from-bracket"></i>
                         Keluar
                     </a>
                 </li>
