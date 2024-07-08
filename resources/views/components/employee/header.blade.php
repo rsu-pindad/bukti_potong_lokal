99e316256faf29dfbc5a35e485ebbe2f98308391
rsu-pindad <sautomated01@gmail.com>
2024-07-08 14:51:33 +0700

    delete unused element

--- resources/views/components/employee/header.blade.php
+++ resources/views/components/employee/header.blade.php
@@ -3,18 +3,9 @@
 
     <ul class="navbar-nav flex-row d-md-none">
         <li class="nav-item text-nowrap">
-            <button class="nav-link px-3 text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSearch" aria-controls="navbarSearch" aria-expanded="false" aria-label="Toggle search">
-                <i class="bi bi-search"></i>
-            </button>
-        </li>
-        <li class="nav-item text-nowrap">
             <button class="nav-link px-3 text-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-                <i class="bi bi-list"></i>
+                <i class="fa-solid fa-bars"></i>
             </button>
         </li>
     </ul>
-
-    <div id="navbarSearch" class="navbar-search w-100 collapse">
-        <input class="form-control w-100 rounded-0 border-0" type="text" placeholder="Cari" aria-label="Search">
-    </div>
 </header>
