1d59d3ae04256b4b3456ddb115f1b4d8e52f7921
rsu-pindad <sautomated01@gmail.com>
2024-07-08 14:49:00 +0700

    add layout icon & add navigation link

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" href="{{ basset(base_path('vendor/twbs/bootstrap/dist/css/bootstrap.min.css')) }}">
-    <link rel="stylesheet" href="{{ basset(base_path('vendor/twbs/bootstrap-icons/font/bootstrap-icons.min.css')) }}">
+    <link rel="stylesheet" href="{{ basset(base_path('vendor/fortawesome/font-awesome/css/all.min.css')) }}">
 
 </head>
 
@@ -17,16 +17,16 @@
         <div class="col-lg-3 col-md-5 col-sm-8 col-xs-12 m-auto p-5">
             <div class="card">
                 <div class="card-header">
-                    <h3 class="text-center m-0">PPH21</h3>
+                    <h3 class="text-center m-0"><i class="fa-solid fa-file-invoice-dollar px-2"></i> PPH21</h3>
                 </div>
                 <div class="card-body">
                     <form action="{{ route('auth/authenticate') }}" method="post">
                         @csrf
-                        <label for="" class="form-label">Username</label>
+                        <label for="username" class="form-label">Username</label>
                         <div class="input-group mb-3">
                             <input type="text" class="form-control @error('username') is-invalid @enderror" name="username" id="username" placeholder="Username" value="{{ old('username') }}">
                             <span class="input-group-text" id="basic-addon1">
-                                <i class="bi bi-person-fill"></i>
+                                <i class="fa-solid fa-user"></i>
                             </span>
                             @error('username')
                             <div class="invalid-feedback">
@@ -34,11 +34,11 @@
                             </div>
                             @enderror
                         </div>
-                        <label for="" class="form-label">Password</label>
+                        <label for="password" class="form-label">Password</label>
                         <div class="input-group mb-3">
                             <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="password" placeholder="Password">
                             <span class="input-group-text" id="basic-addon2">
-                                <i class="bi bi-lock-fill"></i>
+                                <i class="fa-solid fa-lock"></i>
                             </span>
                             @error('password')
                             <div class="invalid-feedback">
@@ -51,10 +51,16 @@
                         </div>
                     </form>
                 </div>
+                <div class="card-footer">
+                    <a href="{{route('cari-index')}}">belum punya akun ?</a>
+                </div>
             </div>
         </div>
     </main>
+
     <script src="{{ basset(base_path('vendor/twbs/bootstrap/dist/js/bootstrap.min.js')) }}"></script>
+    <script src="{{ basset(base_path('vendor/fortawesome/font-awesome/js/all.min.js')) }}"></script>
+
 </body>
 
 </html>
