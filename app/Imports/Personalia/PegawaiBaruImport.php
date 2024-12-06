f6e2ea753a60561cc062416761496e08ba1fe8b2
rsu-pindad <sautomated01@gmail.com>
2024-12-06 08:34:28 +0700

    change npp to nullable

--- app/Imports/Personalia/PegawaiBaruImport.php
+++ app/Imports/Personalia/PegawaiBaruImport.php
@@ -3,6 +3,7 @@
 namespace App\Imports\Personalia;
 
 use App\Models\Employee;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Carbon;
 use Maatwebsite\Excel\Concerns\OnEachRow;
 use Maatwebsite\Excel\Concerns\SkipsEmptyRows;
@@ -47,40 +48,47 @@ class PegawaiBaruImport extends \PhpOffice\PhpSpreadsheet\Cell\StringValueBinder
     {
         $rowIndex = $row->getIndex();
         $row      = $row->toArray();
-        // dd($row);
 
-        $nik = Employee::where('nik', $row['nik'])->first();
-        if ($nik) {
-            $nik->npp                = $row['npp'];
-            $nik->npp_baru           = $row['npp_baru'];
-            $nik->nama               = $row['nama'];
-            $nik->status_kepegawaian = $row['status_kepegawaian'];
-            $nik->nik                = $row['nik'];
-            $nik->npwp               = $row['npwp'];
-            $nik->status_ptkp        = $row['status_ptkp'];
-            $nik->email              = $row['email'];
-            $nik->no_hp              = $row['no_hp'];
-            $nik->tmt_masuk          = $row['tmt_masuk'] == null ? null : Carbon::parse(Date::excelToDateTimeObject($row['tmt_masuk']))->format('d/m/Y');
-            $nik->tmt_keluar         = $row['tmt_keluar'] == null ? null : Carbon::parse(Date::excelToDateTimeObject($row['tmt_keluar']))->format('d/m/Y');
-            $nik->save();
+        try {
+            $nik = Employee::where('nik', $row['nik'])->first();
+            if ($nik) {
+                $nik->npp                = $row['npp'];
+                $nik->npp_baru           = $row['npp_baru'];
+                $nik->nama               = $row['nama'];
+                $nik->status_kepegawaian = $row['status_kepegawaian'];
+                $nik->nik                = $row['nik'];
+                $nik->npwp               = $row['npwp'];
+                $nik->status_ptkp        = $row['status_ptkp'];
+                $nik->email              = $row['email'];
+                $nik->no_hp              = $row['no_hp'];
+                $nik->tmt_masuk          = $row['tmt_masuk'] == null ? null : Carbon::parse(Date::excelToDateTimeObject($row['tmt_masuk']))->format('d/m/Y');
+                $nik->tmt_keluar         = $row['tmt_keluar'] == null ? null : Carbon::parse(Date::excelToDateTimeObject($row['tmt_keluar']))->format('d/m/Y');
+                $nik->save();
 
-            return false;
+                return false;
+            }
+
+            $store = Employee::insert([
+                'npp'                => $row['npp'],
+                'npp_baru'           => $row['npp_baru'],
+                'nama'               => $row['nama'],
+                'status_kepegawaian' => $row['status_kepegawaian'],
+                'nik'                => $row['nik'],
+                'npwp'               => $row['npwp'],
+                'status_ptkp'        => $row['status_ptkp'],
+                'email'              => $row['email'],
+                'no_hp'              => $row['no_hp'],
+                'tmt_masuk'          => $row['tmt_masuk'] == null ? null : Carbon::parse(Date::excelToDateTimeObject($row['tmt_masuk']))->format('d/m/Y'),
+                'tmt_keluar'         => $row['tmt_keluar'] == null ? null : Carbon::parse(Date::excelToDateTimeObject($row['tmt_keluar']))->format('d/m/Y'),
+                'created_at'         => Carbon::now(),
+            ]);
+
+            Log::debug($store);
+
+            return $store;
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
         }
-
-        return Employee::insert([
-            'npp'                => $row['npp'],
-            'npp_baru'           => $row['npp_baru'],
-            'nama'               => $row['nama'],
-            'status_kepegawaian' => $row['status_kepegawaian'],
-            'nik'                => $row['nik'],
-            'npwp'               => $row['npwp'],
-            'status_ptkp'        => $row['status_ptkp'],
-            'email'              => $row['email'],
-            'no_hp'              => $row['no_hp'],
-            'tmt_masuk'          => $row['tmt_masuk'] == null ? null : Carbon::parse(Date::excelToDateTimeObject($row['tmt_masuk']))->format('d/m/Y'),
-            'tmt_keluar'         => $row['tmt_keluar'] == null ? null : Carbon::parse(Date::excelToDateTimeObject($row['tmt_keluar']))->format('d/m/Y'),
-            'created_at'         => Carbon::now(),
-        ]);
     }
 
     public function batchSize(): int
@@ -90,6 +98,6 @@ class PegawaiBaruImport extends \PhpOffice\PhpSpreadsheet\Cell\StringValueBinder
 
     public function chunkSize(): int
     {
-        return 1000;
+        return 300;
     }
 }
