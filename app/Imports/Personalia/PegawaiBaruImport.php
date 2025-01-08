c69c23024955ec680ef56a852f32a12af18e2f0d
rsu-pindad <sautomated01@gmail.com>
2025-01-08 15:50:23 +0700

    hot fixes

--- app/Imports/Personalia/PegawaiBaruImport.php
+++ app/Imports/Personalia/PegawaiBaruImport.php
@@ -4,41 +4,18 @@ namespace App\Imports\Personalia;
 
 use App\Models\Employee;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Concerns\OnEachRow;
-use Maatwebsite\Excel\Concerns\SkipsEmptyRows;
-use Maatwebsite\Excel\Concerns\WithBatchInserts;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
 use Maatwebsite\Excel\Concerns\WithCustomValueBinder;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithSkipDuplicates;
-use Maatwebsite\Excel\Concerns\WithStartRow;
-use Maatwebsite\Excel\Concerns\WithUpserts;
 use Maatwebsite\Excel\Row;
 use PhpOffice\PhpSpreadsheet\Shared\Date;
-use PhpOffice\PhpSpreadsheet\Shared\StringHelper;
-use DateTime;
 
 class PegawaiBaruImport extends \PhpOffice\PhpSpreadsheet\Cell\StringValueBinder implements OnEachRow, WithCustomValueBinder, WithHeadingRow, WithChunkReading, WithSkipDuplicates
 {
     protected $mulai;
     protected $akhir;
-
-    // public function __construct($mulai, $akhir)
-    // {
-    //     $this->mulai = $mulai;
-    //     $this->akhir = $akhir;
-    // }
-
-    // public function startRow(): int
-    // {
-    //     return $this->mulai;
-    // }
-
-    // public function limit(): int
-    // {
-    //     return $this->akhir;
-    // }
 
     public function uniqueBy()
     {
@@ -50,42 +27,26 @@ class PegawaiBaruImport extends \PhpOffice\PhpSpreadsheet\Cell\StringValueBinder
         $rowIndex = $row->getIndex();
         $row      = $row->toArray();
 
-        $tmt_masuk        = null;
-        $tmt_keluar       = null;
-        $tmt_masuk_parse  = null;
-        $tmt_keluar_parse = null;
-        $final_masuk      = null;
-        $final_keluar     = null;
-        // dd(Date::excelToDateTimeObject($row['tmt_masuk']));
+        $tmt_masuk    = null;
+        $tmt_keluar   = null;
+        $final_masuk  = null;
+        $final_keluar = null;
 
-        // try {
-        if ($row['tmt_masuk'] != null) {
-            $tmt_masuk = Date::excelToDateTimeObject($row['tmt_masuk']);
-
-            // $date_masuk = new DateTime($tmt_masuk);
-            // // Convert the DateTime object to a string using the format() method
-            // $final_masuk = $date_masuk->format('Y-m-d H:i:s');
-
-            // $date_keluar = new DateTime($tmt_keluar);
-            // // Convert the DateTime object to a string using the format() method
-            // $final_keluar = $date_keluar->format('Y-m-d H:i:s');
-            // $tmt_masuk_parse  = Carbon::createFromFormat('d/m/Y H:i', $tmt_masuk);
-            // $tmt_keluar_parse = Carbon::createFromFormat('d/m/Y H:i', $tmt_keluar);
-            $final_masuk = Carbon::parse($tmt_masuk)->format('Y-m-d');
-            // dd($final_masuk);
-            // $tmt_masuk  = Carbon::createFromFormat('d/m/Y H:i', $row['tmt_masuk']);
-            // $tmt_keluar = Carbon::createFromFormat('d/m/Y H:i', $row['tmt_keluar']);
-        } else {
-            $tmt_masuk = null;
-        }
-        // if ($row['tmt_masuk'] != null) {
-        //     $tmt_keluar   = Date::excelToDateTimeObject($row['tmt_keluar']);
-        //     $final_keluar = Carbon::parse($tmt_keluar)->format('Y-m-d');
-        // } else {
-        //     $tmt_keluar = null;
-        // }
         $nik = Employee::where('nik', $row['nik'])->first();
         if ($nik) {
+            if ($row['tmt_masuk'] != null) {
+                $tmt_masuk   = Date::excelToDateTimeObject($row['tmt_masuk']);
+                $final_masuk = Carbon::parse($tmt_masuk)->format('Y-m-d');
+            } else {
+                $tmt_masuk = null;
+            }
+            if ($row['tmt_masuk'] != null) {
+                $tmt_keluar   = Date::excelToDateTimeObject($row['tmt_keluar']);
+                $final_keluar = Carbon::parse($tmt_keluar)->format('Y-m-d');
+            } else {
+                $tmt_keluar = null;
+            }
+
             $nik->npp                = $row['npp'];
             $nik->npp_baru           = $row['npp_baru'];
             $nik->nama               = $row['nama'];
@@ -96,7 +57,7 @@ class PegawaiBaruImport extends \PhpOffice\PhpSpreadsheet\Cell\StringValueBinder
             $nik->email              = $row['email'];
             $nik->no_hp              = $row['no_hp'];
             $nik->tmt_masuk          = $final_masuk;
-            $nik->tmt_keluar         = null;
+            $nik->tmt_keluar         = $final_keluar;
             $nik->save();
 
             return false;
@@ -113,16 +74,11 @@ class PegawaiBaruImport extends \PhpOffice\PhpSpreadsheet\Cell\StringValueBinder
             'email'              => $row['email'],
             'no_hp'              => $row['no_hp'],
             'tmt_masuk'          => $final_masuk,
-            'tmt_keluar'         => null,
+            'tmt_keluar'         => $final_keluar,
             'created_at'         => Carbon::now(),
         ]);
 
-        Log::debug($store);
-
         return $store;
-        // } catch (\Throwable $th) {
-        //     Log::error($th->getMessage());
-        // }
     }
 
     public function batchSize(): int
