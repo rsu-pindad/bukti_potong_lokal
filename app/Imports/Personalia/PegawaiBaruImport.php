cb31bc91d3e7b806212fa73702694ebb8cd3baba
rsu-pindad <sautomated01@gmail.com>
2025-01-08 09:07:45 +0700

    fix

--- app/Imports/Personalia/PegawaiBaruImport.php
+++ app/Imports/Personalia/PegawaiBaruImport.php
@@ -47,44 +47,50 @@ class PegawaiBaruImport extends \PhpOffice\PhpSpreadsheet\Cell\StringValueBinder
     public function onRow(Row $row)
     {
         $rowIndex = $row->getIndex();
-        $row = $row->toArray();
+        $row      = $row->toArray();
 
-        // dd($row);
+        $tmt_masuk  = null;
+        $tmt_keluar = null;
 
         // try {
-        $tmt_masuk = $row['tmt_masuk'] == null ? null : Carbon::createFromFormat('d/m/Y', $row['tmt_masuk'])->toDateTimeString;
-        $tmt_keluar = $row['tmt_keluar'] == null ? null : Carbon::createFromFormat('d/m/Y', $row['tmt_keluar'])->toDateTimeString;
+        if ($row['tmt_masuk'] != null) {
+            $tmt_masuk  = Carbon::createFromFormat('d/m/Y', $row['tmt_masuk'])->toDateTimeString;
+            $tmt_keluar = Carbon::createFromFormat('d/m/Y', $row['tmt_keluar'])->toDateTimeString;
+        } else {
+            $tmt_masuk  = null;
+            $tmt_keluar = null;
+        }
         $nik = Employee::where('nik', $row['nik'])->first();
         if ($nik) {
-            $nik->npp = $row['npp'];
-            $nik->npp_baru = $row['npp_baru'];
-            $nik->nama = $row['nama'];
+            $nik->npp                = $row['npp'];
+            $nik->npp_baru           = $row['npp_baru'];
+            $nik->nama               = $row['nama'];
             $nik->status_kepegawaian = $row['status_kepegawaian'];
-            $nik->nik = $row['nik'];
-            $nik->npwp = $row['npwp'];
-            $nik->status_ptkp = $row['status_ptkp'];
-            $nik->email = $row['email'];
-            $nik->no_hp = $row['no_hp'];
-            $nik->tmt_masuk = $row['tmt_masuk'] == null ? null : Carbon::parse($tmt_masuk)->format('Y-m-d');
-            $nik->tmt_keluar = $row['tmt_keluar'] == null ? null : Carbon::parse($tmt_keluar)->format('Y-m-d');
+            $nik->nik                = $row['nik'];
+            $nik->npwp               = $row['npwp'];
+            $nik->status_ptkp        = $row['status_ptkp'];
+            $nik->email              = $row['email'];
+            $nik->no_hp              = $row['no_hp'];
+            $nik->tmt_masuk          = $row['tmt_masuk'] == null ? null : Carbon::parse($tmt_masuk)->format('Y-m-d');
+            $nik->tmt_keluar         = $row['tmt_keluar'] == null ? null : Carbon::parse($tmt_keluar)->format('Y-m-d');
             $nik->save();
 
             return false;
         }
 
         $store = Employee::insert([
-            'npp' => $row['npp'],
-            'npp_baru' => $row['npp_baru'],
-            'nama' => $row['nama'],
+            'npp'                => $row['npp'],
+            'npp_baru'           => $row['npp_baru'],
+            'nama'               => $row['nama'],
             'status_kepegawaian' => $row['status_kepegawaian'],
-            'nik' => $row['nik'],
-            'npwp' => $row['npwp'],
-            'status_ptkp' => $row['status_ptkp'],
-            'email' => $row['email'],
-            'no_hp' => $row['no_hp'],
-            'tmt_masuk' => $row['tmt_masuk'] == null ? null : Carbon::parse($tmt_masuk)->format('Y-m-d'),
-            'tmt_keluar' => $row['tmt_keluar'] == null ? null : Carbon::parse($tmt_keluar)->format('Y-m-d'),
-            'created_at' => Carbon::now(),
+            'nik'                => $row['nik'],
+            'npwp'               => $row['npwp'],
+            'status_ptkp'        => $row['status_ptkp'],
+            'email'              => $row['email'],
+            'no_hp'              => $row['no_hp'],
+            'tmt_masuk'          => $row['tmt_masuk'] == null ? null : Carbon::parse($tmt_masuk)->format('Y-m-d'),
+            'tmt_keluar'         => $row['tmt_keluar'] == null ? null : Carbon::parse($tmt_keluar)->format('Y-m-d'),
+            'created_at'         => Carbon::now(),
         ]);
 
         Log::debug($store);
