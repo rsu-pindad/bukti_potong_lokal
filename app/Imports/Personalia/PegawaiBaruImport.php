66dcf446d9657a24df9d316ef899c3570507273c
rsu-pindad <sautomated01@gmail.com>
2025-01-08 08:40:40 +0700

    fix format date

--- app/Imports/Personalia/PegawaiBaruImport.php
+++ app/Imports/Personalia/PegawaiBaruImport.php
@@ -52,8 +52,8 @@ class PegawaiBaruImport extends \PhpOffice\PhpSpreadsheet\Cell\StringValueBinder
         // dd($row);
 
         // try {
-        $tmt_masuk = $row['tmt_masuk'] == null ? null : Carbon::createFromFormat('d/m/Y', $row['tmt_masuk']);
-        $tmt_keluar = $row['tmt_keluar'] == null ? null : Carbon::createFromFormat('d/m/Y', $row['tmt_keluar']);
+        $tmt_masuk = $row['tmt_masuk'] == null ? null : Carbon::createFromFormat('d/m/Y', $row['tmt_masuk'])->format('Y-m-d');
+        $tmt_keluar = $row['tmt_keluar'] == null ? null : Carbon::createFromFormat('d/m/Y', $row['tmt_keluar'])->format('Y-m-d');
         $nik = Employee::where('nik', $row['nik'])->first();
         if ($nik) {
             $nik->npp = $row['npp'];
