5399b47ac4fab517b96e068eb66d46262b67ab00
rsu-pindad <sautomated01@gmail.com>
2025-02-01 21:43:28 +0700

    fix export nik

--- app/Exports/Pajak/EpinKaryawanExport.php
+++ app/Exports/Pajak/EpinKaryawanExport.php
@@ -3,31 +3,78 @@
 namespace App\Exports\Pajak;
 
 use App\Models\Employee;
-use Illuminate\Contracts\View\View;
 use Illuminate\Support\Carbon;
-use Maatwebsite\Excel\Concerns\FromView;
+use Illuminate\Contracts\View\View;
+// use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use Maatwebsite\Excel\Concerns\WithCustomValueBinder;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
-class EpinKaryawanExport implements FromView
+class EpinKaryawanExport extends \PhpOffice\PhpSpreadsheet\Cell\StringValueBinder implements FromCollection, WithHeadings, ShouldAutoSize, WithColumnFormatting, WithCustomValueBinder
+
 {
-    public function view(): View
+    // public function view(): View
+    // {
+    //     return view('pajak.exports.epin-employee')->with([
+    //         'employees' => Employee::select([
+    //             'npp',
+    //             'npp_baru',
+    //             'nik',
+    //             'nama',
+    //             'status_ptkp',
+    //             'status_kepegawaian',
+    //             'npwp',
+    //             'email',
+    //             'no_hp',
+    //             'epin'
+    //         ])
+    //         // ->where('status_kepegawaian', 'Tetap')
+    //         //   ->orWhere('status_kepegawaian', 'Kontrak')
+    //           ->get(),
+    //     ]);
+    // }
+
+    public function collection()
     {
-        return view('pajak.exports.epin-employee')->with([
-            'employees' => Employee::select([
-                'npp',
-                'npp_baru',
-                'nik',
-                'nama',
-                'status_ptkp',
-                'status_kepegawaian',
-                'npwp',
-                'email',
-                'no_hp',
-                'epin'
-            ])
-            // ->where('status_kepegawaian', 'Tetap')
-            //   ->orWhere('status_kepegawaian', 'Kontrak')
-              ->get(),
-        ]);
+        $employee = Employee::select([
+            'npp',
+            'npp_baru',
+            'nik',
+            'nama',
+            'status_ptkp',
+            'status_kepegawaian',
+            'npwp',
+            'email',
+            'no_hp',
+            'epin'
+        ])->get();
+        return $employee;
+    }
+
+    public function headings(): array
+    {
+        return [
+            'npp',
+            'npp_baru',
+            'nik',
+            'nama',
+            'status_ptkp',
+            'status_kepegawaian',
+            'npwp',
+            'email',
+            'no_hp',
+            'epin'
+        ];
+    }
+
+    public function columnFormats(): array
+    {
+        return [
+            'C' => NumberFormat::FORMAT_TEXT
+        ];
     }
 
     public function properties(): array
